67cc45863bb2c50685a7aabdb03bbeeb22742722
IamThng <ttthhhaaannnggg@gmail.com>
2019-07-12 13:40:24 +0700

    change directory

--- __init__.py
+++ __init__.py
@@ -1,7 +1,4 @@
-from flask import Flask, render_template, request,jsonify
-
-import aiml
-
+from flask import *
 import os
 
 app = Flask(__name__)
@@ -9,7 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
-    return  render_template('index.html')
+    return "Hello World."
+
+@app.route('/home',methods=['POST','GET'])
+
+def home():
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        return render_template('index.html')
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=3000)
+    app.debug = True
+    app.run()
